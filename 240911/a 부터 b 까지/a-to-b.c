9be1fcd8f904ff870c404219bf479c36d1cbd736
LemonEclipse <kmg8022@naver.com>
2024-09-11 00:03:04 +0900

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240911/a 부터 b 까지/a-to-b.c
+++ 240911/a 부터 b 까지/a-to-b.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int a,b;
-    scanf("%d %d", &a,&b);
-    printf("%d ", a);
-    while(a<b){
-        
-        if(a%2==1){
-            a*=2;
-            printf("%d ", a);
-            if(a>b){
-                break;
-            }
-        }
-        else if(a%2==0){
-            a+=3;
-            printf("%d ", a);
-            if(a>b){
-                break;
-            }
-        }
+    // 변수 선언
+    int a, b, i;
 
-    }
+	// 입력
+	scanf("%d %d", &a, &b);
+	
+	i = a;
+    
+    // 출력
+	while(i <= b) {
+		printf("%d ", i);
+		if(i % 2 == 1)
+			i *= 2;
+		else
+			i += 3;
+	}
+
     return 0;
 }
