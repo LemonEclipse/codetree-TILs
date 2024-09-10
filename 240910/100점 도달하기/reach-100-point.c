14d1289052b86c784eae806f6ea8ffb45dd1fe0b
LemonEclipse <kmg8022@naver.com>
2024-09-10 23:30:56 +0900

    Title: 100점 도달하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240910/100점 도달하기/reach-100-point.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    for (int i=n;i<=100;i++){
+        if(i>=90){
+            printf("A ")
+        }
+        else if(90>i>=80){
+            printf("B ")
+        }
+    }
+    return 0;
+}
