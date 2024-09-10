e2152f3afc73df840dfe0fbda45a046afd68b1aa
LemonEclipse <kmg8022@naver.com>
2024-09-10 23:58:13 +0900

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240910/a 부터 b 까지/a-to-b.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d %d", &a,&b);
+    printf("%d ", a);
+    while(a<b){
+        
+       
+        if(a%2==1){
+            a*=2;
+            printf("%d ", a);
+        }
+        else if(a%2==0){
+            a+=3;
+            printf("%d ", a);
+        }
+
+    }
+    return 0;
+}
