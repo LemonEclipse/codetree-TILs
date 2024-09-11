ce781648f710aa5f8a5bb553862ea0fe026faf48
LemonEclipse <kmg8022@naver.com>
2024-09-11 12:21:49 +0900

    Title: a의 배수들의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240911/a의 배수들의 곱/product-of-multiples-of-a.c
+++ 240911/a의 배수들의 곱/product-of-multiples-of-a.c
@@ -4,8 +4,8 @@ int main() {
     int a,b,i=1,prod=1;
     scanf("%d %d", &a,&b);
     for(i;i<=b;i++){
-        if(a%i==0){
-            prod*=i
+        if(i%a==0){
+            prod*=i;
         }
     }
     printf("%d", prod);
