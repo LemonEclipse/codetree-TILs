148155947c6d4efc673ac4cda89ab79d6648b984
LemonEclipse <kmg8022@naver.com>
2024-09-13 14:47:25 +0900

    Title: 두 개의 직각삼각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240913/두 개의 직각삼각형/two-right-triangle.c
+++ 240913/두 개의 직각삼각형/two-right-triangle.c
@@ -2,25 +2,19 @@
 
 int main() {
     int n;
-    scanf("%d", &n);  // 사용자로부터 정수 n 입력
+    scanf("%d", &n); 
 
     for(int i = 0; i < n; i++) {
-        // 왼쪽 삼각형의 별 출력
+        
         for(int j = 0; j < n - i; j++) {
             printf("*");
         }
-        
-        // 중간 공백 출력
         for(int j = 0; j < 2 * i; j++) {
             printf(" ");
         }
-        
-        // 오른쪽 삼각형의 별 출력
         for(int j = 0; j < n - i; j++) {
             printf("*");
         }
-        
-        // 줄바꿈
         printf("\n");
     }
     
