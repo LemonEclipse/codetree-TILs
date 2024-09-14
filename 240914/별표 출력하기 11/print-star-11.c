c867aeea13b452ebce53d17d0d72d05fa68a59eb
LemonEclipse <kmg8022@naver.com>
2024-09-14 12:42:22 +0900

    Title: 별표 출력하기 11, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240914/별표 출력하기 11/print-star-11.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n,i,j;
+    scanf("%d", &n);
+    for(i=1;i<=(2*n+1);i++){
+        for(j=1;j<=(2*n+1);j++){
+            if(i%2==0 && j%2==0){
+                printf("  ");
+            }
+            else{
+                printf("* ");
+            }
+            
+        }
+        printf("\n");
+    }
+    return 0;
+}
