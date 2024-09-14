c70da053f6f1ebcfda5522bbcb89258656ac4b5b
LemonEclipse <kmg8022@naver.com>
2024-09-14 16:43:25 +0900

    Title: 행에 따라 다르게 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240914/행에 따라 다르게 출력/output-differently-for-row.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n,i,j;
+    int cnt=1;
+    scanf("%d", &n);
+
+	for (i = 1; i <=n; i++) {
+		if(i%2!=0){
+            for(j=1;j<=n;j++){
+                printf("%d ",cnt);
+                cnt++;
+            }
+        }
+        else{
+            for(j=1;j<=n;j++){
+                printf("%d ", cnt+1);
+                cnt+=2;
+            }
+            cnt+=1;  /* 이게 중요*/
+        }
+		printf("\n");
+	}
+
+	return 0;
+
+}
