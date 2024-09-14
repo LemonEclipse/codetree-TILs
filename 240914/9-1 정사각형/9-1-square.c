128d397f00c6ee322b759dccf8d130e922b2c122
LemonEclipse <kmg8022@naver.com>
2024-09-14 15:29:54 +0900

    Title: 9-1 정사각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240914/9-1 정사각형/9-1-square.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+	int cnt = 9;
+    int n,i,j;
+    scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			printf("%d", cnt);
+            if(cnt==1){
+                cnt=9;
+            }
+			cnt--;
+		}
+		printf("\n");
+	}
+
+	return 0;
+
+}
