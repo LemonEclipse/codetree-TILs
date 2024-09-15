cd6a257140ec38d9e029b0a324cd270a5f5e73dc
LemonEclipse <kmg8022@naver.com>
2024-09-16 00:57:13 +0900

    Title: 3n + 1 수열 m번, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240916/3n + 1 수열 m번/3n-plus-1-sequence-m-times.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n,cnt=0,i;
+
+    scanf("%d", &n);
+
+    for(i=1;i<=n;i++){
+        scanf("%d", &n);
+        while (n != 1) {
+            if (n % 2 == 0) {  
+                n /= 2;
+            } else {           
+                n = n * 3 + 1;
+            }
+            cnt++;
+        }
+        printf("%d\n", cnt);
+    }    
+
+    
+    return 0;
+}
