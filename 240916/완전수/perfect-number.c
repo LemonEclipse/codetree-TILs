4f50ac916783a6178f8f37cb37df9ce49c427dd7
LemonEclipse <kmg8022@naver.com>
2024-09-16 01:20:50 +0900

    Title: 완전수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240916/완전수/perfect-number.c
+++ 240916/완전수/perfect-number.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int start,end,i,sum=0,cnt=0,j;
+    int start,end,i,cnt=0,j;
     scanf("%d %d", &start,&end);
 
     for(i=start;i<=end;i++){
+        int sum=0;
         for(j=1;j<=i;j++){
             if(i%j==0 &&i!=j){
                 sum+=j;
