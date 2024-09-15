1b335b7143a65a782e661e820bf05525995e0671
LemonEclipse <kmg8022@naver.com>
2024-09-16 01:36:01 +0900

    Title: 약수가 세 개인 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240916/약수가 세 개인 수/numbers-with-three-divisors.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int start,end,i,cnt=0,j;
+    scanf("%d %d", &start,&end);
+
+    for(i=start;i<=end;i++){
+        int sum=0;
+        for(j=1;j<=i;j++){
+            if(i%j==0 &&i!=j){
+                sum+=1;
+            }
+        }
+        if(sum==3){
+            cnt+=1;
+        }
+    }
+    printf("%d", cnt);
+    return 0;
+}
