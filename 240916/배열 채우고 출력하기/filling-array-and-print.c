8e00ddcdb3fc0b4cd230a861e3b14916b6dafb3c
LemonEclipse <kmg8022@naver.com>
2024-09-16 15:51:22 +0900

    Title: 배열 채우고 출력하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240916/배열 채우고 출력하기/filling-array-and-print.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+	char arr[10];
+	int val, sum_val;
+	sum_val = 0;
+	for (int i = 0; i < 10; i++) {
+		scanf("%c", &arr[i]);
+		
+	}
+	for(int i = 9;i>=0;i--){
+        printf("%c ",arr[i]);
+    }
+
+	return 0;
+
+}
