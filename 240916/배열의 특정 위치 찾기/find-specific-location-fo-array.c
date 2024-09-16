ed67c17466b31236f362ceba41b77ee7ddc29beb
LemonEclipse <kmg8022@naver.com>
2024-09-16 21:16:50 +0900

    Title: 배열의 특정 위치 찾기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240916/배열의 특정 위치 찾기/find-specific-location-fo-array.c
+++ 240916/배열의 특정 위치 찾기/find-specific-location-fo-array.c
@@ -5,7 +5,7 @@ int main() {
     
     int arr[10];
     for(i=0;i<10;i++){
-        scanf(" %d", &arr[i]);
+        scanf("%d", &arr[i]);
         if(i%2!=0){
             sum_val+=arr[i];
         }
