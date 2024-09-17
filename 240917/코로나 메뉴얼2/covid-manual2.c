8955aaac1747d197ebcf78271cd485042faad64a
LemonEclipse <kmg8022@naver.com>
2024-09-17 22:38:19 +0900

    Title: 코로나 메뉴얼2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240917/코로나 메뉴얼2/covid-manual2.c
+++ 240917/코로나 메뉴얼2/covid-manual2.c
@@ -4,7 +4,7 @@ int main() {
     int arr[3],arr2[4],i;
     char arr_c[3];
     for(i=0;i<3;i++){
-        scanf("%c %d", &arr_c[i],&arr[i]);
+        scanf(" %c %d", &arr_c[i],&arr[i]);
         if(arr_c[i]=='Y'){
                 if(arr[i]>=37){
                     arr2[0]++;
