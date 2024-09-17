72e7f0106fc8f540b4f39624586151429bc0e608
LemonEclipse <kmg8022@naver.com>
2024-09-17 23:12:52 +0900

    Title: 나눗셈의 나머지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240917/나눗셈의 나머지/remainder-of-division.c
+++ 240917/나눗셈의 나머지/remainder-of-division.c
@@ -1,38 +1,28 @@
 #include <stdio.h>
+#include <stdio.h>
 
 int main() {
-    int a, b, cnt = 0, c_arr[11] = {0}, sum = 0;
+    int a, b;
+    int remainder_count[10] = {0};  // 나머지는 0부터 b-1까지 나올 수 있으므로 b는 10 이하이므로 최대 10칸 배열 선언
+    int sum = 0;
+
+    // a와 b 입력받기
     scanf("%d %d", &a, &b);
 
-    int temp_a = a;  // a 값을 유지하기 위해 복사본 사용
-
-    // 몫이 0이 될 때까지 나누기
-    while (temp_a > 0) {
-        temp_a /= b;
-        cnt++;
+    // a가 1보다 클 때까지 반복
+    while (a > 1) {
+        int remainder = a % b;  // 나머지 계산
+        remainder_count[remainder]++;  // 해당 나머지 등장 횟수 카운트
+        a /= b;  // a를 b로 나눈 몫으로 갱신
     }
 
-    int arr[cnt];  // cnt 크기의 배열 선언
-
-    // 나머지를 구하는 과정
-    for (int i = 0; i < cnt; i++) {
-        int k = a % b;
-        arr[i] = k;
-        a /= b;  // a를 갱신
-    }
-
-    // 나머지를 카운팅하는 배열 생성
-    for (int i = 0; i < cnt; i++) {
-        c_arr[arr[i]]++;
-    }
-
-    // c_arr 배열에서 제곱합 계산
-    for (int i = 0; i < 10; i++) {
-        sum += c_arr[i] * c_arr[i];  // 누적 합계 계산
+    // 등장 횟수의 제곱합 계산
+    for (int i = 0; i < b; i++) {
+        sum += remainder_count[i] * remainder_count[i];  // 제곱합 계산
     }
 
     // 결과 출력
-    printf("%d", sum);
+    printf("%d\n", sum);
 
     return 0;
 }
