ea3c50d6364f39284576c7dec412591cf5cf77a3
LemonEclipse <kmg8022@naver.com>
2024-09-17 16:15:43 +0900

    Title: 짝수인 것만 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240917/짝수인 것만 출력하기/print-even-number.c
+++ 240917/짝수인 것만 출력하기/print-even-number.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int n,i,arr[10],N_arr[i];
+    int n,i;
     scanf("%d", &n);
+    int arr[n];
     for(i=0;i<n;i++){
+        int N_arr[i];
         scanf("%d", &arr[i]);
         if(arr[i]%2==0){
             N_arr[i]=arr[i];
