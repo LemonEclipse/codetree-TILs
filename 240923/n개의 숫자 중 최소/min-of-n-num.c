932d7475d98e0c14390709cebb8ba7e603c6b7ae
LemonEclipse <kmg8022@naver.com>
2024-09-23 16:48:26 +0900

    Title: n개의 숫자 중 최소, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240923/n개의 숫자 중 최소/min-of-n-num.c
+++ 240923/n개의 숫자 중 최소/min-of-n-num.c
@@ -4,16 +4,16 @@ int main() {
     int n,i,cnt=0;
     scanf("%d", &n);
     int arr[n];
-    for(int k=1;k<=n;k++){
+    for(int k=0;k<n;k++){
         scanf("%d", &arr[k]);
     }
     int min=arr[0];
-    for(i=1;i<=n;i++){
+    for(i=0;i<n;i++){
         if(arr[i]<min){
             min=arr[i];
         }
     }
-    for(i=1;i<=n;i++){
+    for(i=0;i<n;i++){
         if(arr[i]==min){
             cnt++;
         }
