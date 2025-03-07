b235775d87ac16897ad49b57b53f48119b0e7f5a
LemonEclipse <kmg8022@naver.com>
2025-03-07 16:26:18 +0900

    Title: 두 개의 격자 비교하기, Time: 79ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250307/두 개의 격자 비교하기/compare-two-grid.py
+++ 250307/두 개의 격자 비교하기/compare-two-grid.py
@@ -1,4 +1,4 @@
-N,M = map(int,input().split())
+N,M = map(int,input().split(" "))
 
 arr_1 = [
     [0 for _ in range(M)]
@@ -9,11 +9,11 @@ arr_2 = [
     for _ in range(N)
 ]
 arr_1 = [
-    list(map(int,input().split()))
+    list(map(int,input().split(" ")))
     for _ in range(4)
 ]
 arr_2 = [
-    list(map(int,input().split()))
+    list(map(int,input().split(" ")))
     for _ in range(4)
 ]
 
