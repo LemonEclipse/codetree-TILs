ec054faeca76b18f7808ece0cbda46f00d8d7a10
LemonEclipse <kmg8022@naver.com>
2025-03-08 19:45:37 +0900

    Title: 격자로 사각형 만들기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250308/격자로 사각형 만들기/print-grid-in-rectangle.py
+++ 250308/격자로 사각형 만들기/print-grid-in-rectangle.py
@@ -1,16 +1,16 @@
 n = int(input())
 
 arr = [
-    [0 for _ in range(5)]
-    for _ in range(5)
+    [0 for _ in range(n)]
+    for _ in range(n)
 ]
-for i in range(5):
+for i in range(n):
     arr[0][i] = 1
     arr[i][0] = 1
 
 # 나머지 칸을 채움
-for i in range(1, 5):
-    for j in range(1, 5):
+for i in range(1, n):
+    for j in range(1, n):
         arr[i][j] = arr[i - 1][j] + arr[i][j - 1] + arr[i-1][j-1]
 
 # 출력
