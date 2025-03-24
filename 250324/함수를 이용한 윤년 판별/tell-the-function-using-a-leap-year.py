1e1bfd2bdcd86befcb2e706f2f0782801b353e4f
LemonEclipse <kmg8022@naver.com>
2025-03-24 18:05:37 +0900

    Title: 함수를 이용한 윤년 판별, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250324/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 250324/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,18 +1,28 @@
+# 변수 선언 및 입력:
 y = int(input())
 
-# Please write your code here.
-def leep_year(year):
+
+def is_leap_year(y):
+    # 4의 배수가 아니라면 윤년이 확실히 아닙니다.
     if y % 4 != 0:
-        return false
+        return False
     
+    # 여기까지 온 이상 4의 배수임을 가정해도 됩니다.
+    # 그 중 100의 배수가 아니라면 확실히 윤년입니다.
     if y % 100 != 0:
-        return true
+        return True
     
+    # 여기까지 온 이상 100의 배수임을 가정해도 됩니다.
+    # 그 중 400의 배수라면 확실히 윤년입니다.
     if y % 400 == 0:
-        return true
+        return True
     
-    return false
-if leep_year(y):
+    # 여기까지 온 이상 100의 배수이지만, 400의 배수가 아닙니다.
+    # 따라서 확실히 윤년이 아닙니다.
+    return False
+
+
+if is_leap_year(y):
     print("true")
 else:
     print("false")
