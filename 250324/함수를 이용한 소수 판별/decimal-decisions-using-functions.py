449a3b744a4f23fa1107a06cc6ca420dee7dda08
LemonEclipse <kmg8022@naver.com>
2025-03-24 17:57:41 +0900

    Title: 함수를 이용한 소수 판별, Time: 74ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250324/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 250324/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -3,7 +3,8 @@ a, b = map(int, input().split())
 # Please write your code here.
 def dec_sum(a,b):
     Sum = 0
-    
+    if a==b:
+        return = 0
     for i in range(a,b+1):
         Bool = True
         for j in range(2,i):
