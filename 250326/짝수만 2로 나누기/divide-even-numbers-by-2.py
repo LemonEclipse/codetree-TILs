cafcfdbbe35ac5697610769ad264936da2240955
LemonEclipse <kmg8022@naver.com>
2025-03-26 15:54:08 +0900

    Title: 짝수만 2로 나누기, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250326/짝수만 2로 나누기/divide-even-numbers-by-2.py
+++ 250326/짝수만 2로 나누기/divide-even-numbers-by-2.py
@@ -4,9 +4,10 @@ arr = list(map(int, input().split()))
 # Please write your code here.
 def even_div(arr):
     for i in range(len(arr)):
-        if L[i]%2==0:
-            L[i] //=2
+        if arr[i]%2==0:
+            arr[i] //=2
     return arr
 
 even_div(arr)
-print(arr)
+for i in arr:
+    print(i, end = " ")
