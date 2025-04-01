3dbf6974aac1623ac11460705bcb6bb491cc0f6e
LemonEclipse <kmg8022@naver.com>
2025-04-01 19:49:50 +0900

    Title: Top K 숫자 구하기, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250401/Top K 숫자 구하기/kth-number.py
+++ 250401/Top K 숫자 구하기/kth-number.py
@@ -1,7 +1,8 @@
 nums = list(map(int,input().split()))
-
+arr = list(map(int,input().split()))
 L = []
-for i in range(nums[0]):
+for i in arr:
+
     L.append(i)
 L.sort()
-print(L[nums[1]+1])
+print(L[nums[1]-1])
